c70d3556163debea8ebc60d2d7d7100056d485d9
Izu99 <isuruumanga123@gmail.com>
2024-05-22 19:31:27 +0530

    update code admin productlist page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './customer/Home';
-import Login from './customer/Login';
-import Biscuits from './customer/Biscuits';
-import Frozenfoods from './customer/Frozenfoods';
-import ShoppingCart from './customer/ShoppingCart';
-import Signup from './customer/Signup';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./customer/Home";
+import Login from "./customer/Login";
+import Biscuits from "./customer/Biscuits";
+import Frozenfoods from "./customer/Frozenfoods";
+import ShoppingCart from "./customer/ShoppingCart";
+import Signup from "./customer/Signup";
 import Welcome from "./customer/Welcome";
 import AdminDashboard from "./management/AdminDashboard";
 import AdminBiscuits from "./management/AdminBiscuits";
 import AddProducts from "./management/AddProducts";
 import Beverages from "./customer/Beverages";
 import Pasta from "./customer/Pasta";
-import { CartProvider } from './Context/CartContext';
+import { CartProvider } from "./Context/CartContext";
 import EditProduct from "./management/EditProduct";
 import AdminFrozen from "./management/AdminFrozen";
 import AdminBeverages from "./management/AdminBeverages";
@@ -24,38 +24,38 @@ import AdminProductList from "./management/AdminProductList";
 import CategoryPage from "./management/CategoryPage";
 
 function App() {
-  return (
-    <div>
-      <CartProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/Welcome" element={<Welcome />} />
-            <Route path="/Home" element={<Home />} />
-            <Route path="/Login" element={<Login />} />
-            <Route path="/Biscuits" element={<Biscuits />} />
-            <Route path="/FrozenFoods" element={<Frozenfoods />} />
-            <Route path="/ShoppingCart" element={<ShoppingCart />} />
-            <Route path="/Signup" element={<Signup />} />
-            <Route path="/AdminDashboard" element={<AdminDashboard />} />
-            <Route path="/AdminBiscuits" element={<AdminBiscuits />} />
-            <Route path="/AdminFrozen" element={<AdminFrozen />} />
-            <Route path="/AdminBeverages" element={<AdminBeverages />} />
-            <Route path="/AdminPasta" element={<AdminPasta />} />
-            <Route path="/AddProducts" element={<AddProducts />} />
-            <Route path="/Beverages" element={<Beverages />} />
-            <Route path="/Pasta" element={<Pasta />} />
-            <Route path="/EditProduct/:category/:productId" element={<EditProduct />} />
-            <Route path="/Orders" element={<Orders />} />
-            <Route path="/ProductInventory" element={<ProductInventory />} />
-            <Route path="/CategoryForm" element={<CategoryForm />} />
-            <Route path="/AdminProductList" element={<AdminProductList />} />
-            <Route path="/category/:categoryId" element={<CategoryPage />} />
-            <Route path="/admin/category/:categoryId" component={AdminProductList} />
-          </Routes>
-        </BrowserRouter>
-      </CartProvider>
-    </div>
-  );
+	return (
+		<div>
+			<CartProvider>
+				<BrowserRouter>
+					<Routes>
+						<Route path='/Welcome' element={<Welcome />} />
+						<Route path='/Home' element={<Home />} />
+						<Route path='/Login' element={<Login />} />
+						<Route path='/Biscuits' element={<Biscuits />} />
+						<Route path='/FrozenFoods' element={<Frozenfoods />} />
+						<Route path='/ShoppingCart' element={<ShoppingCart />} />
+						<Route path='/Signup' element={<Signup />} />
+						<Route path='/AdminDashboard' element={<AdminDashboard />} />
+						<Route path='/AdminBiscuits' element={<AdminBiscuits />} />
+						<Route path='/AdminFrozen' element={<AdminFrozen />} />
+						<Route path='/AdminBeverages' element={<AdminBeverages />} />
+						<Route path='/AdminPasta' element={<AdminPasta />} />
+						<Route path='/AddProducts' element={<AddProducts />} />
+						<Route path='/Beverages' element={<Beverages />} />
+						<Route path='/Pasta' element={<Pasta />} />
+						<Route path='/EditProduct/:category/:productId' element={<EditProduct />} />
+						<Route path='/Orders' element={<Orders />} />
+						<Route path='/ProductInventory' element={<ProductInventory />} />
+						<Route path='/CategoryForm' element={<CategoryForm />} />
+						<Route path='/AdminProductList' element={<AdminProductList />} />
+						<Route path='/category/:categoryId' element={<CategoryPage />} />
+						<Route path='/admin/category/:categoryId' element={<AdminProductList />} />
+					</Routes>
+				</BrowserRouter>
+			</CartProvider>
+		</div>
+	);
 }
 
 export default App;
